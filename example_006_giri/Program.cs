d207797f7d79a5c91665d828533d973b8c3620df
cutulhu <pronin199567@gmail.com>
2023-01-25 00:55:17 +0300

    add giri

---
+++ example_006_giri/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 5;
+int c = 3;
+int d = 8;
+int e = 2;
+
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.Write("Самое большое число - ");
+Console.WriteLine(max);

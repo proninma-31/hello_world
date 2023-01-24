3cf5354fa77a3a4fcd3463a0b4092c38b26c136a
cutulhu <pronin199567@gmail.com>
2023-01-25 00:06:40 +0300

    add user cutulhu

---
+++ example_005_master/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи свое имя смертный! ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "ктулху")
+{
+    Console.WriteLine("Да славится повелитель!");
+}
+else 
+{
+   Console.Write("Пх’нглуи мглв’нафх Ктулху Р’льех вгах’нагл фхтагн"); 
+}
